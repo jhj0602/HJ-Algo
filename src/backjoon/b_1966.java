fd607c13093be8e92de4af38bc507a9c9e6ff0b2
jhj960918 <jhj960918@gmail.com>
2021-04-18 21:21:09 +0900

    no message

--- src/backjoon/b_1966.java
+++ src/backjoon/b_1966.java
@@ -10,7 +10,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class b_1966 {
+public class B_1966 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
--- src/backjoon/b_8958.java
+++ src/backjoon/b_8958.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class b_8958 {
+public class B_8958 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
