146b7e2d0dcd717055cf529e0d05fce2c672fcfb
진형준 <jhj960918@gmail.com>
2021-08-06 11:20:21 +0900

    셔플하기 큐사용

--- src/ssafy/expert/academy/SW1208.java
+++ src/ssafy/expert/academy/SW1208.java
@@ -2,30 +2,48 @@ package ssafy.expert.academy;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class SW1208 {
 
 	public static void main(String[] args) throws Exception {
 		StringBuilder sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		for (int test_case = 1; test_case <= 10; test_case++) {
-			int dump = Integer.parseInt(br.readLine());
-			int[] arr = new int[100];
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < 100; i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int N = Integer.parseInt(br.readLine());
+			String[] strArr = br.readLine().split(" ");
+			Queue<String> firstQ = new LinkedList<>();
+			Queue<String> secondQ = new LinkedList<>();
+			int halfLen = 0;
+			if (N % 2 == 0) {
+				halfLen = strArr.length / 2;
+
+			} else {
+				halfLen = strArr.length / 2 + 1;
 			}
-			
-			for (int i = 0; i < dump; i++) {
-				Arrays.sort(arr);
-				arr[0]++;
-				arr[99]--;
+
+			for (int i = 0; i < halfLen; i++) {
+				firstQ.offer(strArr[i]);
 			}
-			Arrays.sort(arr);
-			int dis = arr[99] - arr[0];
-			sb.append("#" + test_case + " " + dis + "\n");
+			for (int i = halfLen; i < strArr.length; i++) {
+				secondQ.offer(strArr[i]);
+			}
+			sb.append("#"+test_case+" ");
+			while (true) {
+				if (!firstQ.isEmpty()||!secondQ.isEmpty()) {
+					sb.append(firstQ.poll() + " ");
+					sb.append(secondQ.poll() + " ");
+				}
+				
+				if (firstQ.isEmpty() && secondQ.isEmpty()) {
+					break;
+				}
+
+			}
+			sb.append("\n");
+
 		}
 		br.close();
 		System.out.println(sb);
