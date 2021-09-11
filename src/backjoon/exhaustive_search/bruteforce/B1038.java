ab5c3b1fd8e17641407f5374d73a239527b3883d
jjj03231 <jhj960918@gmail.com>
2021-09-11 23:34:14 +0900

    no message

---
+++ src/backjoon/exhaustive_search/bruteforce/B1038.java
@@ -0,0 +1,28 @@
+package backjoon.exhaustive_search.bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B1038 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		boolean [] arr = new boolean[1000001];
+		int count = 0;
+		for (int i = 0; i <= arr.length; i++) {
+			if (i % 10 < 10) {
+				count++;
+			} else {
+
+			}
+
+			if (count - 1 == n) {
+				System.out.println(i);
+				break;
+			}
+		}
+	}
+
+}
