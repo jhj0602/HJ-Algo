ae4905d0f3aa755ddf6ce6f43431316b4cfaeb5c
jjj03231 <jhj960918@gmail.com>
2021-09-11 23:34:26 +0900

    no message

---
+++ src/backjoon/twopointer/B11728.java
@@ -0,0 +1,31 @@
+package backjoon.twopointer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B11728 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[] a = new int[n];
+		int[] b = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			a[i] = Integer.parseInt(st.nextToken());
+		}
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < m; i++) {
+			b[i] = Integer.parseInt(st.nextToken());
+		}
+		int [] arr = new int[n+m];
+		int aCount=0,bCount=0;
+		int idx =0;
+		
+	}
+
+}
