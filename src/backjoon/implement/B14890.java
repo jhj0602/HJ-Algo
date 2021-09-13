6cf5a2cafe1462acefe8a75bdff83088780921b1
jjj03231 <jhj960918@gmail.com>
2021-09-13 18:48:31 +0900

    implement

--- src/backjoon/implement/B14890.java
+++ src/backjoon/implement/B14890.java
@@ -7,80 +7,67 @@ import java.util.StringTokenizer;
 
 public class B14890 {
 	static int[][] map;
-	static boolean[][] visit;// 경사로를 놓은곳에는 또 놓을수 없다.
+	static boolean[][] visit;
 	static int n, l;
 
-	// 오른쪽으로 갈거
-	// 아래쪽을 갈거
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		l = Integer.parseInt(st.nextToken());
-		map = new int[n + 1][n + 1];
-		visit = new boolean[n + 1][n + 1];
-		for (int i = 1; i < n + 1; i++) {
+		map = new int[n][n];
+		visit = new boolean[n][n];
+		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j < n + 1; j++) {
+			for (int j = 0; j < n; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-
 			}
 		}
 		int cnt = 0; // 길의 갯수
-		for (int i = 1; i <= n; i++) {// 오른쪽으로 갈꺼
-			int tempCnt = 0;// 이게 n이 되면 cnt++
-			for (int j = 1; j < n; j++) {
-				if (map[i][j] == map[i][j + 1]) {
-					tempCnt++;
-					continue;
-				}
-				if (Math.abs(map[i][j] - map[i][j + 1]) == 1) {// 차이가 1
-					if (!rightCheck(i, j)) {// 경사로 체크
-						System.out.println(i);
-						break;
-					}
-					tempCnt++;
-				} else if (Math.abs(map[i][j] - map[i][j + 1]) > 1) {// 차이가 2
-					System.out.println(i);
-					break;
-				}
+		for (int i = 0; i < n; i++) {// 0이 오른쪽 //1이 아래쪽
+			if (check(i, 0, 0)) {
+				cnt++;
 			}
-			if (tempCnt == n - 1) {
+			if (check(0, i, 1)) {
 				cnt++;
 			}
 		}
-		System.out.println("cnt" + cnt);
+		System.out.println(cnt);
 
 	}
 
-	private static boolean rightCheck(int x, int y) {// 오른쪽으로 가는거
-
-		if (y - l > 0 && y + l < n + 1) {
-	
-				if (map[x][y] > map[x][y + 1]) {// 아래로 경사로
-					int temp = map[x][y + 1];// 경사질때 아래 있는 값
-					for (int i = 1; i < l; i++) {
-						if (temp != map[x][y + i] && !visit[x][y + i]) {
-							return false;
-						}
-System.out.println("zz");
-						visit[x][y + i] = true;
-					}
-				} else {// 위로 경사로
-					int temp = map[x][y];
-					for (int i = 0; i < l; i++) {
-						if (temp != map[x][y - i] && !visit[x][y - i]) {
-							return false;
-						}
-						visit[x][y - i] = true;
-					}
-
-				}
-				return true;
+	private static boolean check(int x, int y, int dir) {// 오른쪽으로 가는거
+		int[] height = new int[n];
+		boolean[] visit = new boolean[n];
+		for (int i = 0; i < n; i++) {
+			height[i] = (dir == 0) ? map[x][y + i] : map[x + i][y];
+		}
+		for (int i = 0; i < n - 1; i++) {
+			if (height[i] == height[i + 1]) {
+				continue;
 			}
-			
-		
-		return false;
+			if (Math.abs(height[i] - height[i + 1]) == 1) {// 차이가 1일 경우이다
+				if (height[i] + 1 == height[i + 1]) {// 아래에서 위로 경사로이다
+					for (int j = i; j > i - l; j--) {
+						if (j < 0 || height[j] != height[i] || visit[j]) {
+							return false;
+						}
+						visit[j] = true;
+					}
+				} else {
+					for (int j = i + 1; j <= i + l; j++) {
+						if (j >= n || height[j] != height[i + 1] || visit[j]) {
+							return false;
+						}
+						visit[j] = true;
+					}
+				}
+			} else if (Math.abs(height[i] - height[i + 1]) > 1) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 }
