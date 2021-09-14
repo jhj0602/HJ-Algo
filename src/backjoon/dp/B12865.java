19eaa3f93a3fe83a4fb2b51efeb639e1639ce635
jjj03231 <jhj960918@gmail.com>
2021-09-14 16:07:50 +0900

    평범한 배낭 dp 최적해 구하기

---
+++ src/backjoon/dp/B12865.java
@@ -0,0 +1,41 @@
+package backjoon.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B12865 {
+	static int n, k;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());// 목표무게
+		int[] weights = new int[n + 1];
+		int[] profits = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			weights[i] = Integer.parseInt(st.nextToken());
+			profits[i] = Integer.parseInt(st.nextToken());
+		}
+		int[][] dp = new int[n + 1][k + 1];
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= k; j++) {
+
+				if (weights[i] <= j) {// 해당물건을 가방에 넣을 수 있다.
+					dp[i][j] = Math.max(dp[i - 1][j], profits[i] + dp[i - 1][j - weights[i]]);
+
+				} else {//해당 물건을 가방에 넣을수 없다.
+					dp[i][j] = dp[i - 1][j];
+				}
+				System.out.print(dp[i][j]+" ");
+			}
+			System.out.println();
+		}
+		System.out.println(dp[n][k]);
+
+	}
+
+}
