0c505e411c67c99d3de7f2a48fa39ca7a859554a
jjj03231 <jhj960918@gmail.com>
2021-09-15 22:52:57 +0900

    최소 스패닝 트리 크루스칼

--- src/ssafy/onlin_learn/MSTKruskalTest.java
+++ src/ssafy/onlin_learn/MSTKruskalTest.java
@@ -3,35 +3,33 @@ package ssafy.onlin_learn;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class MSTKruskalTest {
 
-	static class Edge implements Comparable<Edge> {
-		int start, end, weight;
+	static class Edge {
+		int start, end;
+		long weight;
 
-		public Edge(int start, int end, int weight) {
+		public Edge(int start, int end, long weight) {
 			this.start = start;
 			this.end = end;
 			this.weight = weight;
 		}
-
-		@Override
-		public int compareTo(Edge o) {
-			return Integer.compare(this.weight, o.weight);// 오른차순의 결과
-		}
-
 	}
 
 	static int V, E;
-	static Edge[] edgeList;
+	static ArrayList<Edge> edgeList;
 	static int[] parents;
 
 	private static void make() {
-		parents = new int[V];
+		parents = new int[V + 1];
 		// 모든 원소를 자신을 대표자로 만듦
-		for (int i = 0; i < V; i++) {
+		for (int i = 0; i <= V; i++) {
 			parents[i] = i;
 		}
 	}
@@ -54,28 +52,42 @@ public class MSTKruskalTest {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		// 간선 리스트 작성
-		edgeList = new Edge[E];
-		for (int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine());
-			edgeList[i] = new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
-					Integer.parseInt(st.nextToken()));
-		}
-		Arrays.sort(edgeList);
-		make();// 모든 정점을 각각으로 집합으로 만들고 출발
-		// 간선 하나씩 시도하며 트리 만들어감
-		int result = 0, cnt = 0;
-		for (Edge edge : edgeList) {
-			if (union(edge.start, edge.end)) {
-				result += edge.weight;
-				if (++cnt == V - 1)
-					break;
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case <= T; test_case++) {
+
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			V = Integer.parseInt(st.nextToken());
+			E = Integer.parseInt(st.nextToken());
+			// 간선 리스트 작성
+			edgeList = new ArrayList<Edge>();
+			for (int i = 0; i < E; i++) {
+				st = new StringTokenizer(br.readLine());
+				edgeList.add(new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
+						Long.parseLong(st.nextToken())));
 			}
+			Collections.sort(edgeList, new Comparator<Edge>() {
+				@Override
+				public int compare(Edge o1, Edge o2) {
+					// TODO Auto-generated method stub
+					return Long.compare(o1.weight, o2.weight);
+				}
+			});
+			make();// 모든 정점을 각각으로 집합으로 만들고 출발
+			// 간선 하나씩 시도하며 트리 만들어감
+			int result = 0, cnt = 0;
+			for (Edge edge : edgeList) {
+				if (union(edge.start, edge.end)) {
+					result += edge.weight;
+					if (++cnt == V - 1)
+						break;
+				}
+			}
+			sb.append("#" + test_case + " " + result+"\n");
+
 		}
-		System.out.println(result);
+		System.out.println(sb);
+
 	}
 
 }
