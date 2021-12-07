3ec5f1324d726cb3b22d6356861ce246ab9223da
jhj960918 <jhj960918@gmail.com>
2021-12-07 23:41:03 +0900

    no message

---
+++ src/programmers/P_12899.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class P_12899 {
+
+	public static void main(String[] args) {
+		solution(11);
+
+	}
+
+	public static String solution(int n) {
+		String answer = "";
+		return answer;
+	}
+}
