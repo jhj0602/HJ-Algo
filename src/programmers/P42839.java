88951b7023202fcd63d75cfa489ee44f51d20850
jhj960918 <jhj960918@gmail.com>
2021-12-08 23:45:46 +0900

    완전탐색 소수찾기 Level2

---
+++ src/programmers/P42839.java
@@ -0,0 +1,56 @@
+//완전탐색/소수찾기
+package programmers;
+
+import java.util.HashSet;
+
+public class P42839 {
+	static char[] numberArr = {};
+	static char[] temp;
+	static boolean[] visit;
+	static int count;
+	static HashSet<Integer> set = new HashSet<>();
+
+	public static void main(String[] args) {
+		System.out.println(solution("17"));
+	}
+
+	public static int solution(String numbers) {
+		numberArr = numbers.toCharArray();
+		temp = new char[numbers.length()];
+		visit = new boolean[numbers.length()];
+		combination(0, "");
+		return set.size();
+	}
+
+	public static void combination(int cnt, String str) {
+		if (str != "") {
+			int num = Integer.parseInt(str);
+			if (isPrime(num)) {
+				set.add(num);
+			}
+		}
+
+		if (cnt == numberArr.length) {
+			return;
+		}
+
+		for (int i = 0; i < numberArr.length; i++) {
+			temp[cnt] = numberArr[i];
+			if (visit[i])
+				continue;
+			temp[cnt] = numberArr[i];
+			visit[i] = true;
+			combination(cnt + 1, str + temp[cnt]);
+			visit[i] = false;
+		}
+	}
+
+    public static boolean isPrime(int num){ //소수 판별
+        if(num==1||num==0) return false;
+        for(int i=2; i<=Math.sqrt(num); i++){
+            if(num%i==0) return false;
+        }
+        
+        return true;
+    }
+}
