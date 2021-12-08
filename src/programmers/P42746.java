0242a63e9feb634b62a55ea13a1e793924bbb57c
jhj960918 <jhj960918@gmail.com>
2021-12-08 21:07:23 +0900

    정렬(가장 큰 수) Level2

---
+++ src/programmers/P42746.java
@@ -0,0 +1,30 @@
+// 정렬/가장큰 수 Level2
+package programmers;
+
+import java.util.Arrays;
+
+public class P42746 {
+
+	public static void main(String[] args) {
+		int[] numbers = { 6, 10, 2 };
+		System.out.println(solution(numbers));
+	}
+
+	public static String solution(int[] numbers) {
+		String answer = "";
+		String[] str = new String[numbers.length];
+
+		for (int i = 0; i < numbers.length; i++) {
+			str[i] = String.valueOf(numbers[i]);
+		}
+		Arrays.sort(str, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+		if (str[0].equals("0")) {
+			return "0";
+		}
+		for (String string : str) {
+			answer += string;
+		}
+
+		return answer;
+	}
+}
