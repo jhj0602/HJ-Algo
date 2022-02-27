0e0dd7f36501cc1d4d7a85613d9ac639d9efb013
jhj0602 <jhj960918@gmail.com>
2022-02-27 17:37:33 +0900

    Level1 : 신고 결과 받기

--- src/programmers/level1/P92334.java
+++ src/programmers/level1/P92334.java
@@ -8,23 +8,42 @@ public class P92334 {
         P92334 p = new P92334();
         String[] id_list = {"muzi", "frodo", "apeach", "neo"};
         String[] report = {"muzi frodo", "apeach frodo", "frodo neo",
-                "muzi frodo","muzi neo", "apeach muzi"};
+                "muzi neo", "muzi neo", "apeach muzi"};
         int k = 2;
-        p.solution(id_list, report, k);
+
+        String[] id_list2 = {"con", "ryan"};
+        String[] report2 = {"ryan con", "ryan con", "ryan con", "ryan con"};
+        int k2 = 3;
+
+        System.out.println(p.solution(id_list, report, k));
+        p.solution(id_list2, report2, k2);
     }
 
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = {};
+        int[] answer = new int[id_list.length];
+        HashMap<String, Integer> idMap = new HashMap<>();
+        for (String s : id_list) {
+            idMap.put(s, 0);
+        }
         HashMap<String, Integer> reportUser = new HashMap<>();
         HashMap<String, Integer> reportUserCount = new HashMap<>();
         for (String s : report) {
             reportUser.put(s, reportUser.getOrDefault(s, 0) + 1);
+            String[] str = s.split(" ");
+            if (reportUser.get(s) == 1) {
+                reportUserCount.put(str[1], reportUserCount.getOrDefault(str[1], 0) + 1);
+            }
         }
-        System.out.println(reportUser);
-        System.out.println("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+        for (String s : reportUser.keySet()) {
+            String[] str = s.split(" ");
+            if (reportUserCount.get(str[1]) >= k) {
+                idMap.put(str[0], idMap.get(str[0]) + 1);
+            }
+        }
 
-
-        System.out.println();
+        for (int i = 0; i < id_list.length; i++) {
+            answer[i] = idMap.get(id_list[i]);
+        }
         return answer;
     }
 }
