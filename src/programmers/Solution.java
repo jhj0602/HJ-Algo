86e647de77a721f2528d1a543e42c896761eb021
jhj0602 <jhj960918@gmail.com>
2022-03-19 01:41:29 +0900

    Level 2 : 다리를 지나는 트럭

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -1,67 +1,47 @@
-package programmers;// 다음과 같이 import를 사용할 수 있습니다.
+package programmers;
 
-import java.util.*;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Solution {
-    static class Dot {
-        int x;
-        int y;
 
-        Dot(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+    @Test
+    public void 정답() {
+        Solution s = new Solution();
+        int[] truck_weights = {7, 4, 5, 6};
+        System.out.println(s.solution(2, 10, truck_weights));
+
     }
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
-    public int solution(int[][] garden) {
-        // 여기에 코드를 작성해주세요.
+
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
-        Queue<Dot> q = new LinkedList<>();
-        int cnt=0;
-        int len = garden.length;
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < len; j++) {
-                if (garden[i][j] == 1) {
-                    q.offer(new Dot(i, j));
-                    cnt++;
-                }
-            }
-        }
-        while (cnt < len * len) {
-            Queue<Dot> tempQ = new LinkedList<>();
-            answer++;
-            for (int k = 0; k < q.size(); k++) {
-                Dot dot = q.poll();
-                for (int i = 0; i < 4; i++) {
-                    int cx = dot.x + dx[i];
-                    int cy = dot.y + dy[i];
-                    if (cx >= 0 && cy >= 0 && cx < len && cy < len && garden[cx][cy] == 0) {
-                        garden[cx][cy] = 1;
-                        cnt++;
-                        tempQ.offer(new Dot(cx, cy));
+        Queue<Integer> q = new LinkedList<>();
+        int sum =0;
+        int time =0;
+        for (int w : truck_weights ) {
+            while (true){
+                if(q.isEmpty()) {
+                    q.offer(w);
+                    sum += w;
+                    time++;
+                }else if(q.size()==bridge_length){
+                    sum-=q.poll();
+                }else{
+                    if(sum+w>weight){
+                        q.offer(0);
+                        time++;
+                    }else{
+                        q.offer(w);
+                        sum+=w;
+                        time++;
+                        break;
                     }
                 }
             }
-            q =tempQ;
+
         }
-        return answer;
-    }
-
-    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        int[][] garden1 = {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
-        int ret1 = sol.solution(garden1);
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
-
-        int[][] garden2 = {{1, 1}, {1, 1}};
-        int ret2 = sol.solution(garden2);
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("solution 메소드의 반환 값은 " + ret2 + " 입니다.");
-
+        return time+bridge_length;
     }
 }
