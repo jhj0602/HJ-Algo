b509e112337c67ecfd04e9d9a9dea2008ff993e0
jhj0602 <jhj960918@gmail.com>
2022-04-26 18:33:29 +0900

    Solve : 위장

---
+++ src/programmers/Level2/P42578.java
@@ -0,0 +1,16 @@
+package programmers.level2;
+
+import org.junit.Test;
+
+public class P42578 {
+    @Test
+    public void 위장() {
+        String[][] clothes = {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        solution(clothes);
+    }
+
+    public int solution(String[][] clothes) {
+        int answer = 0;
+        return answer;
+    }
+}
