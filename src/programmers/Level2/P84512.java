71e7cdcc5a0a91ef253e178389334c395a4bd73e
jhj0602 <jhj960918@gmail.com>
2022-04-28 20:32:12 +0900

    Solve : 단어모음

--- src/programmers/Level2/P84512.java
+++ src/programmers/Level2/P84512.java
@@ -3,57 +3,45 @@ package programmers.level2;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 
 public class P84512 {
     static char[] alphabet = {'A', 'E', 'I', 'O', 'U'};
     static boolean[] isSelected;
+    static String[] temp;
     static ArrayList<String> list = new ArrayList<>();
 
     @Test
     public void 정답답() throws Exception {
-        solution("AAAAE");
+//        solution("AAAAE");
+//        solution("AAAE");
+       solution("EIO");
+//        solution("I");
 
     }
 
     public int solution(String word) {
         int answer = 0;
         isSelected = new boolean[5];
-        subset(0);
+        for (int i = 1; i <= 5; i++) {
+            temp = new String[i];
+            permutation(0, i);
+        }
         Collections.sort(list);
+//        System.out.println(list);
+        System.out.println(list.indexOf(word)+1);
         return answer;
     }
 
-    private static void subset(int cnt) {// 부분집합 구하기
-        if (cnt == 5) {// 하나의 부분집합이 완성 됐을 때 할일.
-            String temp = "";
-            for (int i = 0; i < 5; i++) {
-                temp += isSelected[i] ? alphabet[i] : "";
-            }
-            System.out.println(temp);
-            list.add(temp);
+
+    private static void permutation(int cnt, int N) {
+        if (cnt == N) {
+            list.add(String.join("", temp));
             return;
         }
-
-        // 현재 원소를 부분집합에 넣기
-        isSelected[cnt] = true;
-        subset(cnt + 1);
-        // 현재 원소를 부분집합에 넣지 않기
-        isSelected[cnt] = false;
-        subset(cnt + 1);
+        for (int i = 0; i < 5; i++) {
+            temp[cnt] = String.valueOf(alphabet[i]);
+            permutation(cnt + 1, N);
+        }
     }
 }
-
-
-//    private static void comb(int cnt) {//중복조합
-//        if (cnt == 5) {
-//            System.out.println(Arrays.toString(temp));
-//            return;
-//        }
-//        for (int i = 0; i < 5; i++) {
-//            temp[cnt] =alphabet[i];
-//            comb(cnt + 1);
-//
-//        }
-//    }
